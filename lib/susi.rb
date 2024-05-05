d6187dc4e2c0c64f6b70317cf38ef72ccc113740
Daniel Bovensiepen <oss@bovi.li>
2024-05-06 00:38:01 +0800

    feat: add VNC access

--- lib/susi.rb
+++ lib/susi.rb
@@ -1,8 +1,54 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
+require 'socket'
+require 'timeout'
+require 'json'
+require 'webrick'
 
 module Susi
+  class VNC
+    def self.open(name)
+      vm = VM.new(name)
+      port = vm.vnc_www_port
+      ip = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }
+      token = (0...8).map { (65 + rand(26)).chr }.join
+      url = "http://#{ip.ip_address}:#{port}/#{token}.html"
+
+      # prepare paths and files
+      path = File.expand_path(File.dirname(__FILE__))
+      novnc_path = File.join(path, "novnc")
+      html = File.read(File.join(novnc_path, "screen.html"))
+      html.gsub!("###TOKEN###", token)
+      html.gsub!("###HOST###", ip.ip_address)
+      html.gsub!("###PORT###", vm.vnc_websocket_port.to_s)
+
+      server = WEBrick::HTTPServer.new(:Port => port,
+                                       :Logger => WEBrick::Log.new("/dev/null"),
+                                       :AccessLog => [])
+      server.mount_proc "/#{token}.html" do |req, res|
+        # print access by someone on command line
+        puts "[#{Time.now}] Access by: #{req.peeraddr[3]}"
+        res.body = html
+      end
+      server.mount_proc "/#{token}.close.html" do |req, res|
+        puts "[#{Time.now}] Closing VNC..."
+        res.body = "Closing VNC..."
+        server.shutdown
+      end
+      server.mount("/core",
+                   WEBrick::HTTPServlet::FileHandler,
+                   File.join(novnc_path, "core"))
+      server.mount("/vendor",
+                   WEBrick::HTTPServlet::FileHandler,
+                   File.join(novnc_path, "vendor"))
+
+      puts "[#{Time.now}] VNC screen: #{url}"
+
+      server.start
+    end
+  end
+
   class Disk
     def self.create(name, size)
       system("qemu-img create -f qcow2 #{name}.qcow2 #{size}G")
@@ -10,6 +56,126 @@ module Susi
 
     def self.clone(name, clone)
       system("qemu-img create -f qcow2 -F qcow2 -b #{name}.qcow2 #{clone}.qcow2")
+    end
+  end
+
+  class QMP
+    def initialize(port)
+      @port = port
+      @server = TCPSocket.new('localhost', @port)
+
+      resp = JSON.parse(@server.gets)
+      if resp["QMP"]["version"]["qemu"]["major"] == 9
+        puts "QEMU version 9 detected"
+      else
+        puts "QEMU version not supported"
+        exit 1
+      end
+
+      @server.puts('{"execute":"qmp_capabilities"}')
+      resp = JSON.parse(@server.gets)
+      if resp["return"] == {}
+        puts "Connected to QMP socket"
+      else
+        puts "Failed to connect to QMP socket"
+        exit 1
+      end
+    end
+
+    def quit
+      @server.puts('{"execute":"quit"}')
+    end
+  end
+
+  class VM
+    def initialize(name)
+      @name = name
+      @qmp_port = nil
+
+      ps = `ps aux | grep qemu-system-x86_64`
+      ps.split("\n").each do |line|
+        next unless line.match /\-name/
+        m = line.match(/-name (\w+)/)
+        n = m[1]
+        next unless n == @name
+        m = line.match(/-qmp tcp:localhost:(\d+)/)
+        @qmp_port = m[1].to_i
+      end
+
+      #@qmp = QMP.new(@qmp_port)
+    end
+
+    def quit
+      QMP.new(@qmp_port).quit
+    end
+
+    def vnc_port
+      5900
+    end
+
+    def vnc_www_port
+      5800
+    end
+
+    def vnc_websocket_port
+      5700
+    end
+
+    def self.quit(name)
+      q = QMP.new(4444)
+      q.quit
+    end
+
+    def self.install(name, disk, iso)
+      self.start(name, disk, cdrom: iso)
+    end
+
+    def self.start(name, disk, cdrom: nil)
+      cmd = []
+
+      cmd << "qemu-system-x86_64"
+      cmd << "-name #{name}"
+      cmd << "-hda #{disk}.qcow2"
+      cmd << "-daemonize"
+      cmd << "-qmp tcp:localhost:4444,server,nowait"
+      cmd << "-vnc :0,websocket=on"
+
+      if cdrom
+        cmd << "-cdrom #{cdrom}"
+      end
+
+      puts cmd.join(" ")
+      system(cmd.join(" "))
+
+      QMP.new(4444)
+    end
+
+    def self.ls
+      # grep from ps output
+      ps = `ps aux | grep qemu-system-x86_64`
+      ps.split("\n").each do |line|
+        next unless line.match /\-name/
+        m = line.match(/-name (\w+)/)
+        n = m[1]
+        m = line.match(/-hda (.*?)\s/)
+        d = m[1]
+        m = line.match(/-qmp tcp:localhost:(\d+)/)
+        q = m[1]
+        m = line.match(/-vnc :(\d+)/)
+        v = m[1].to_i
+
+        # get local device name or IP
+        ip = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }
+        puts ip.inspect
+
+        puts <<-EOF
+#{n}
+  - Disk: #{d}
+  - QMP: :#{q}
+  - VNC: :#{5900 + v} (Websocket: #{5700 + v})
+  - Screen: http://#{ip.ip_address}:#{5800 + v}/
+EOF
+      end
     end
   end
 end
--- susi.rb
+++ susi.rb
@@ -18,7 +18,68 @@ elsif ARGV[0] == 'disk' && ARGV[1] == 'clone' &&
   new_disk_name = ARGV[3]
   Susi::Disk.clone(disk_name, new_disk_name)
 
+# start VM
+elsif ARGV[0] == 'vm' && ARGV[1] == 'start' &&
+    ARGV[2].is_a?(String) && File.exist?("#{ARGV[2]}.qcow2")
+  vm_name = ARGV[2]
+  Susi::VM.start(vm_name, vm_name)
+
+# install VM
+elsif ARGV[0] == 'vm' && ARGV[1] == 'install' &&
+  ARGV[2].is_a?(String) && File.exist?("#{ARGV[2]}.qcow2") &&
+    ARGV[3].is_a?(String) && File.exist?(ARGV[3])
+  vm_name = ARGV[2]
+  iso = ARGV[3]
+  Susi::VM.install(vm_name, vm_name, iso)
+
+# quit VM
+elsif ARGV[0] == 'vm' && ARGV[1] == 'quit' &&
+    ARGV[2].is_a?(String)
+  vm_name = ARGV[2]
+  Susi::VM.quit(vm_name)
+
+# list VMs
+elsif ARGV[0] == 'vm' && ARGV[1] == 'ls'
+  Susi::VM.ls
+elsif ARGV[0] == 'ls'
+  Susi::VM.ls
+
+# open VNC
+elsif ARGV[0] == 'vnc' &&
+    ARGV[1].is_a?(String)
+  vm_name = ARGV[2]
+  Susi::VNC.open(vm_name)
+
 else
-  puts 'Invalid command'
+  puts <<-EOF
+Invalid command
+
+Usage: 
+  Create a VM disk
+  susi disk create <disk_name> <disk_size>
+
+  Clone a VM disk
+  susi disk clone <disk_name> <new_disk_name>
+
+  Start a VM
+  susi vm start <vm_name>
+
+  Quit a VM
+  susi vm quit <vm_name>
+
+  List VMs
+  susi ls
+  susi vm ls
+
+  Open VNC
+  susi vnc <vm_name>
+
+susi (v0.1.0) - Simple User System Interface
+
+author:  Daniel Bovensiepen
+contact: oss@bovi.li
+www:     https://github.com/bovi/susi
+EOF
+
   exit 1
 end
