e6885ff56f6aa858f40659702419387abbc93665
Daniel Bovensiepen <oss@bovi.li>
2024-10-01 19:40:31 +0800

    chore: rewrite option parsing with optparse

--- lib/susi.rb
+++ lib/susi.rb
@@ -101,7 +101,7 @@ if __FILE__ == $0
     end
 
     def test_cli_empty
-      assert_equal(false, run_command("ruby ./susi.rb"))
+      assert_equal(false, run_command("ruby ./bin/susi"))
     end
 
     def test_create_img
@@ -117,15 +117,15 @@ if __FILE__ == $0
     end
 
     def test_cli_create_img
-      assert_equal(true, run_command("ruby ./susi.rb disk create test 1"))
+      assert_equal(true, run_command("ruby ./bin/susi disk create test 1"))
       assert(File.exist?("test.qcow2"))
       run_command("rm test.qcow2")
     end
 
     def test_cli_create_img_fail
-      assert_equal(false, run_command("ruby ./susi.rb disk create test"))
-      assert_equal(false, run_command("ruby ./susi.rb disk create test pest"))
-      assert_equal(false, run_command("ruby ./susi.rb disk create 1"))
+      assert_equal(false, run_command("ruby ./bin/susi disk create test"))
+      assert_equal(false, run_command("ruby ./bin/susi disk create test pest"))
+      assert_equal(false, run_command("ruby ./bin/susi disk create 1"))
     end
 
     def test_clone_img
@@ -144,19 +144,19 @@ if __FILE__ == $0
     end
 
     def test_cli_clone_img
-      assert_equal(true, run_command("ruby ./susi.rb disk create test 1"))
-      assert_equal(true, run_command("ruby ./susi.rb disk clone test clone"))
+      assert_equal(true, run_command("ruby ./bin/susi disk create test 1"))
+      assert_equal(true, run_command("ruby ./bin/susi disk clone test clone"))
       assert(File.exist?("clone.qcow2"))
       run_command("rm clone.qcow2")
       run_command("rm test.qcow2")
     end
 
     def test_cli_clone_img_fail
-      assert_equal(false, run_command("ruby ./susi.rb disk clone test clone"))
-      assert_equal(true, run_command("ruby ./susi.rb disk create test 1"))
-      assert_equal(false, run_command("ruby ./susi.rb disk clone test"))
-      assert_equal(true, run_command("ruby ./susi.rb disk clone test clone"))
-      assert_equal(false, run_command("ruby ./susi.rb disk clone test clone"))
+      assert_equal(false, run_command("ruby ./bin/susi disk clone test clone"))
+      assert_equal(true, run_command("ruby ./bin/susi disk create test 1"))
+      assert_equal(false, run_command("ruby ./bin/susi disk clone test"))
+      assert_equal(true, run_command("ruby ./bin/susi disk clone test clone"))
+      assert_equal(false, run_command("ruby ./bin/susi disk clone test clone"))
 
       run_command("rm clone.qcow2")
       run_command("rm test.qcow2")
