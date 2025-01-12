f273d5ac101538e28dcf2f762e43e9a9b5b2e997
Daniel Bovensiepen <oss@bovi.li>
2025-01-12 20:05:28 +0800

    feat: enable user network mode during compilation

--- docs/qemu.sh
+++ docs/qemu.sh
@@ -1,7 +1,7 @@
-#apt install python3-venv ninja-build build-essential pkg-config libglib2.0-dev flex bison libpixman-1-dev
+sudo apt install python3-venv ninja-build build-essential pkg-config libglib2.0-dev flex bison libpixman-1-dev
 git clone https://gitlab.com/qemu-project/qemu.git
 cd qemu
 git submodule init
 git submodule update --recursive
-./configure
+./configure --enable-slirp
 make
